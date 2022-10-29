79f6d53f61d8edaa7a0814450c642104b677d882
farhan1611078 <1611078@iub.edu.bd>
2022-10-29 01:15:51 +0100

    A

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 const testRouter = require('./controller/testController.js');
 
-app.listen("443",()=>{
-    console.log(`Server has been started`)
+app.listen(process.env.PORT,()=>{
+    console.log(`Server has been started at port ${process.env.PORT}`)
 })
 
 app.set("view engine","ejs");
